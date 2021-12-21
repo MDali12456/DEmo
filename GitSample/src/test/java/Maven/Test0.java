fea03c6b3a79ae0ef99f33fe9b0594996320d6b8
MDali12456 <mohammedali99335@gmail.com>
2021-12-21 16:22:51 +0530

    Demo

---
+++ GitSample/src/test/java/Maven/Test0.java
@@ -0,0 +1,13 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class Test0 {
+	@Test
+	public void Test0() {
+		System.out.println("hello");
+		
+		System.out.println("java sel");
+	}
+
+}
